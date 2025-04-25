8ddabc4bc14d3e7485ab448c54487a7015c1949e
katesereda1808 <katesereda22@gmail.com>
2025-04-25 09:44:36 +0200

    решение longest substring without repeating с Map

--- medium/longest-substring-without-repeating/longest-substring-without-repeating.js
+++ medium/longest-substring-without-repeating/longest-substring-without-repeating.js
@@ -31,3 +31,32 @@
 // "pwwkew"
 // "ckilbkd"
 // lengthOfLongestSubstring("pwwkew");
+
+
+// решение 2, улучшенное, O(n)
+var lengthOfLongestSubstring2 = function(s) {
+    currMaxLength = 0;
+    const left = 0; // левая граница окна (начало)
+    const seen = new Map();
+
+    for(let right = 0; right < s.length; right++) {
+        // берем каждый символ, s[i]
+        // проверяем есть ли он в мапе, в котором копим пройденные символы и их индексы
+        // если нет, добавляем в seen,
+        // сравниваем длину currSubString с currMaxLength, если она больше то переписываем
+        // если повторяется, тогда 
+        // подвигаем левую сторону окна на индекс повторяющегося символа + 1 (то есть начинаем после повторяющегося символа)
+        let currChar = s[right];
+        // проверяем, что currChar уже встречался в seen и что его индекс входит в пределы рассматриваемого окна
+        if (seen.has(currChar) && seen.get(currChar) >= left) {
+            left = seen.get(currChar) + 1;
+        }
+
+        // Обнови/добавь символ с его последней позицией
+        seen.set(char, right);
+
+        currMaxLength = Math.max(currMaxLength, right - left + 1);
+    }
+    // console.log(currMaxLength);
+    return currMaxLength;
+};
