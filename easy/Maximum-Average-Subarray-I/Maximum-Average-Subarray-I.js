e076f2455f2226c11ede43541427072c8e78b088
katesereda1808 <katesereda22@gmail.com>
2025-04-27 23:41:48 +0200

    Maximum Average Subarray I

---
+++ easy/Maximum-Average-Subarray-I/Maximum-Average-Subarray-I.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var findMaxAverage = function(nums, k) {
+    let left = 0;
+    // let currSum = 0;
+    let currMax = -Infinity; // ! ВАЖНО в массиве могут быть отрицательные значения, поэтому первое значение left должно буть как можно меньше
+    // окно, двигаемся по нему,
+    // при первой итерации считаем сумму в окне и записываем в переменную
+    // при дальнейших итерациях
+    // вычиваем предыдущее значение из суммы
+    // и прибавляем следующее
+    // сравниваем значение с currMax
+    // и обновляем currMax если новое значение больше
+    // правый конец окна равен k - фикс длина подмассива
+
+    // Сначала считаем сумму для первого окна (первые k элементов)
+    let currSum = nums.slice(0, k).reduce((acc, cur) => acc + cur, 0);
+    // или
+    // for (let i = 0; i < k; i++) {
+    //     currSum += nums[i];
+    // }
+    currMax = currSum;
+
+    for (let right = k; right < nums.length; right++) {
+        // считаем сумму подмассива
+        currSum = currSum - nums[right - k] + nums[right];
+        currMax = Math.max(currMax, currSum);
+        left++;
+    };
+    return currMax/k;
+};
